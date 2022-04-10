7d87a3f9cee45823a0fdefeede0b9d8645ddb955
Samir <akhtarsamir242@gmail.com>
2022-04-10 13:45:23 +0530

    first commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2022 All rights reserved, Food House LPU . Developed By - <a href="#"> MD SAMIR AKHTAR</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
